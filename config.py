3617359a9f9b73340a2dbc4a0529b926134f59b1
Nikita669-dot <easypubg30018@gmail.com>
2025-02-19 14:57:24 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+﻿BOT_TOKEN = "7643441349:AAHZ0lSHRSaydmp7ir88PHOhzvr7CCATp6I"  # токен ботика
+CHANNEL_URL = "https://t.me/+otVuVjJNplo1Mzky"  # ссылка канала проверка подписки
+CHANNEL_ID = "-1002219266341"  # айди канала для проверки подписки
+VERIF_CHANNEL_ID = "-1002316926082"  # айди чата для верификации(обсуждение канала с постбеками)
+SUPP = "https://t.me/N_a_m_ber"
+ADMIN_ID = 884976238 # Ваш айди без скобочек
+PROMO = "LoveMines" # промокод
